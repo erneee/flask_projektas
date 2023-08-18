7b54ad1146af3224b5ac23edb36891eb12824951
Ernestas <ernestasvvv@gmail.com>
2023-08-18 15:30:20 +0300

    Pridejom orus

--- flask_app01.py
+++ flask_app01.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return "<h1>Cia yra home page su Flask</h1>"
 
+@app.route("/orai")
+def orai():
+    return "Orai yra per karsti"
+
 @app.route("/<kintamasis>")# cia adreso juosta
 def user(kintamasis):# cia sujungia su adresu
     return f"<h1>Sveiki {kintamasis} atvyke i flask puslapi</h1>"
